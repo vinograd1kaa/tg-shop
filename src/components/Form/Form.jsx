8e38b3f80229c581a1d5fc43bc1a467d830424a7
vinograd1kaa <supermaneee5@gmail.com>
2023-03-06 19:03:43 +0200

    initial

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {useTelegram} from "../../hooks/useTelegram";
 import './Form.css';
 
@@ -6,7 +6,23 @@ const Form = () => {
   const [country, setCountry] = useState('');
   const [street, setStreet] = useState('');
   const [subject, setSubject] = useState('physical');
-  const {tg} = useTelegram();
+  const { tg } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      country,
+      street,
+      subject
+    }
+    tg.sendData(JSON.stringify(data));
+  }, [country, street, subject]);
+
+  useEffect(() => {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    }
+  }, [onSendData])
 
   useEffect(() => {
     tg.MainButton.setParams({
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './ProductList.css';
+import Form from "../Form/Form";
 
 const ProductList = () => {
   return (
     <div>
       PRODUCT LIST
+      <Form />
     </div>
   );
 };
